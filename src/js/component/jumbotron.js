1af9ed6065642b4433db048d1cd6e826e2ffe71f
riduarte <riduarte88@gmail.com>
2020-07-30 13:57:42 +0000

    Jumbotron listo

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropType from "prop-types";
+import "bootstrap";
+//create your first component
+export function Jumbotron(props) {
+	return (
+		<div className="container">
+			<div className="jumbotron">
+				<h1 className="display-4">{props.title}</h1>
+				<p className="lead">{props.text}</p>
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Call action!!
+				</a>
+			</div>
+		</div>
+	);
+}
+Jumbotron.propTypes = {
+	title: PropType.string,
+	text: PropType.string
+};
