7fe89bbe9376108429bd7c5e60cc4268a895d1f7
riduarte <riduarte88@gmail.com>
2020-07-31 08:03:26 +0000

    Trabajo final

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -4,7 +4,7 @@ import "bootstrap";
 //create your first component
 export function Jumbotron(props) {
 	return (
-		<div className="container">
+		<div className="container mt-3">
 			<div className="jumbotron">
 				<h1 className="display-4">{props.title}</h1>
 				<p className="lead">{props.text}</p>
--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,49 @@ import "../styles/index.scss";
 //import your own components
 
 import { Navbar } from "./component/navbar.js";
+import { Jumbotron } from "./component/jumbotron.js";
+import { Card } from "./component/andreacard.js";
+import { Footer } from "./component/andreafooter.js";
 
 //render your react application
-ReactDOM.render(<Navbar />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Navbar />
+		<Jumbotron
+			title="A Warm Welcom!!"
+			text="This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
+		/>
+		<div className="d-flex justify-content-center container">
+			<Card
+				imagenUrl="https://via.placeholder.com/500x325"
+				title="Card title"
+				text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+				buttonUrl="#"
+				button="Go somewhere"
+			/>
+			<Card
+				imagenUrl="https://via.placeholder.com/500x325"
+				title="Card title"
+				text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+				buttonUrl="#"
+				button="Go somewhere"
+			/>
+			<Card
+				imagenUrl="https://via.placeholder.com/500x325"
+				title="Card title"
+				text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+				buttonUrl="#"
+				button="Go somewhere"
+			/>
+			<Card
+				imagenUrl="https://via.placeholder.com/500x325"
+				title="Card title"
+				text="This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+				buttonUrl="#"
+				button="Go somewhere"
+			/>
+		</div>
+		<Footer copyright="Copyright © Your Website" />
+	</div>,
+	document.querySelector("#app")
+);
